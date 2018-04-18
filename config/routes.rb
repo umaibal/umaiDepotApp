b88c2043d8c8dd2fdd1b38f706971eb1c6f48eda
umaibal <umai.balendra@shopify.com>
2018-04-18 19:54:41 -0400

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  
+  # resources :products, shallow: true do
+  #   resources :reviews
+  # end
+
+  # concern :reviewable do
+  #   resources :reviews
+  # end
+
+  # resources :products, concern: :reviewable
+  # resources :users, concern: :reviewable
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
